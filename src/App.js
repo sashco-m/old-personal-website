4707849a7d3b00acbbdb53d3206ab9dfe231355d
Sashco Mistelbacher <sashco.mistelbacher@gmail.com>
2020-10-16 18:34:01 -0400

    updated layout

--- src/App.js
+++ src/App.js
@@ -14,25 +14,30 @@ function App() {
       <CoverImg />
       <Row fluid>
         <Image style={{objectFit:"cover",minHeight:"40vh",opacity:"0%"}} src="https://i.imgur.com/jey24yC.jpg" fluid />
-        <Container className="border" style={{height:"150vh",backgroundColor:"white",zIndex:"10",boxShadow: "0px -14px 32px -8px rgba(0,0,0,0.72)"}} className="" fluid>
+        <Container className="border" style={{backgroundColor:"white",zIndex:"10",boxShadow: "0px -14px 32px -8px rgba(0,0,0,0.72)"}} className="" fluid>
           
           <Row style={{textAlign:"center"}} className="m-1" fluid>
-            <Col lg={5}>
+          <Col className="d-xl-block d-lg-none" xl={1} sm={12}></Col>
+            <Col xl={3} lg={5}>
               <h1 className="m-2" style={{textDecoration:"underline"}}>About</h1>
 
-              <Container className="border rounded " style={{backgroundColor:"white"}}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis enim lobortis scelerisque fermentum dui faucibus. Platea dictumst quisque sagittis purus sit. Quis hendrerit dolor magna eget. Morbi quis commodo odio aenean sed adipiscing diam donec adipiscing. Egestas sed tempus urna et pharetra. Luctus accumsan tortor posuere ac ut consequat semper viverra nam. Diam phasellus vestibulum lorem sed risus ultricies tristique nulla. Viverra nibh cras pulvinar mattis nunc sed blandit. Suspendisse in est ante in nibh mauris cursus. Elit pellentesque habitant morbi tristique senectus et.
-              </Container>
-              <Container className="border rounded mt-3">
-                <Image src="" alt="testimg" />
-              </Container>
+              <Card className="mb-3" style={{textAlign:"left"}} >                   
+              <Card.Body>
+                <Card.Title>Hello!</Card.Title>
+                    <Card.Text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis enim lobortis scelerisque fermentum dui faucibus. Platea dictumst quisque sagittis purus sit. Quis hendrerit dolor magna eget. Morbi quis commodo odio aenean sed adipiscing diam donec adipiscing. Egestas sed tempus urna et pharetra. Luctus accumsan tortor posuere ac ut consequat semper viverra nam. Diam phasellus vestibulum lorem sed risus ultricies tristique nulla. Viverra nibh cras pulvinar mattis nunc sed blandit. Suspendisse in est ante in nibh mauris cursus. Elit pellentesque habitant morbi tristique senectus et.
+                    </Card.Text>
+                  </Card.Body>
+                  <Card.Img variant="bottom" src="https://i.imgur.com/VDpuKi9.jpg/100px180" />
+
+                </Card>
             </Col>
             <Col lg={7}>
               
               <h1 className="m-2" style={{textDecoration:"underline"}}>Hobbies</h1>
               <Row>
               <Col sm={4}>
-                <Card style={{textAlign:"left"}} > 
+                <Card className="mb-3" style={{textAlign:"left"}} > 
                   <Card.Img variant="top" src="https://i.imgur.com/3oqcDP6.jpg" />
                   <Card.Body>
                     <Card.Title>Robotics</Card.Title>
@@ -43,7 +48,7 @@ function App() {
                 </Card>
               </Col>
               <Col sm={4}>
-                <Card style={{textAlign:"left"}} > 
+                <Card className="mb-3" style={{textAlign:"left"}} > 
                   <Card.Img variant="top" src="https://i.imgur.com/Kc7LlMF.jpg" />
                   <Card.Body>
                     <Card.Title>Woodworking</Card.Title>
@@ -54,7 +59,7 @@ function App() {
                 </Card>
               </Col>
               <Col sm={4}>
-                <Card style={{textAlign:"left"}} > 
+                <Card className="mb-3" style={{textAlign:"left"}} > 
                   <Card.Img variant="top" src="https://i.imgur.com/ynJuJ2a.png" />
                   <Card.Body>
                     <Card.Title>Guitar</Card.Title>
@@ -66,6 +71,7 @@ function App() {
               </Col>
               </Row>
             </Col>
+            <Col className="d-xl-block d-lg-none" xl={1} sm={12}></Col>
           </Row>
           
 
