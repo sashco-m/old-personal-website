23ba9429cac2f3d28d278b021a5eeb87cb9b25c3
Sashco Mistelbacher <sashco.mistelbacher@gmail.com>
2020-10-19 23:32:35 -0400

    added a work section

--- src/App.js
+++ src/App.js
@@ -162,11 +162,81 @@ function App() {
                 
                 </Col>
               </Row>
+              <hr className="border"></hr>
+
               
             </Col>
             <Col className="" xl={3} lg={1} ></Col>
           </Row>
           {/* end of projects */}
+          <Row style={{textAlign:""}} className="m-1" fluid>
+            
+            <Col className="" xl={3} lg={1} ></Col>
+              <Col xl={6} lg={10}>
+                
+                <Row>
+                  <Container fluid>
+                    <h1>Work</h1>
+                  </Container>
+                </Row>
+                <Row>
+
+                    <Container fluid>
+                  <h3>Craft Services</h3>
+                    </Container>
+
+                  <Container fluid>
+                  <h6 className="text-muted">Summer 2020</h6>
+                  </Container>
+
+
+                  <Container fluid>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                  </Container>
+ 
+                </Row>
+                <Row>
+
+                    <Container fluid>
+                  <h3>Production Assistant</h3>
+                    </Container>
+
+                  <Container fluid>
+                  <h6 className="text-muted">Summer 2019 and 2020</h6>
+                  </Container>
+
+
+                  <Container fluid>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                  </Container>
+ 
+                </Row>
+                <Row>
+
+                    <Container fluid>
+                  <h3>Location Support</h3>
+                    </Container>
+
+                  <Container fluid>
+                  <h6 className="text-muted">Summer 2019</h6>
+                  </Container>
+
+
+                  <Container fluid>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                  </Container>
+ 
+                </Row>
+
+
+                <hr className="border"></hr>
+              </Col>
+
+                <Col className="" xl={3} lg={1} ></Col>
+            </Row>
+
+
+          {/* end of work */}
 
         {/*end of main container */}
         </Container> 
