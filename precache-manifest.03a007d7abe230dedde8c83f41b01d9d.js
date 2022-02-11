4469641aa5b9b0c08989dd86984dd64712a7b215
Sashco Mistelbacher <sashco.mistelbacher@gmail.com>
2022-02-11 13:38:15 -0500

    Updates

---
+++ precache-manifest.03a007d7abe230dedde8c83f41b01d9d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ece4569fa3da8f073dda792fac74ed09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91c427ecc4e95d5567aa",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d49b1399ae19d72bb745",
+    "url": "/static/css/main.d61c1f3b.chunk.css"
+  },
+  {
+    "revision": "91c427ecc4e95d5567aa",
+    "url": "/static/js/2.1140c0cd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.1140c0cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d49b1399ae19d72bb745",
+    "url": "/static/js/main.5a4b16ae.chunk.js"
+  },
+  {
+    "revision": "d6e7f29aa2787d8456e3",
+    "url": "/static/js/runtime-main.f3499b45.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
